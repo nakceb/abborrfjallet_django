f0431547dfa176f7a196c28e048eb95509bb445b
nakceb <filip.stenbeck@hotmail.com>
2020-04-18 15:25:17 +0200

    moved to svenska sub section

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main)
+    path('', views.main),
+    path('about', views.about_swe)
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -8,3 +8,10 @@ def main(request, *ars, **kwargs):
         return render(request, "frontend/base.html", {})
     except Exception as ex:
         return HttpResponseBadRequest(str(ex))
+
+
+def about_swe(request, *ars, **kwargs):
+    try:
+        return render(request, "frontend/Svenska/about.html", {})
+    except Exception as ex:
+        return HttpResponseBadRequest(str(ex))
